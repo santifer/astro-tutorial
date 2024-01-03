20b6e4281650803857baf9e3038e2371559fa6a1
santifer <almacen@santifer.me>
2024-01-03 19:33:11 +0100

    siguiendo con tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://stirring-meringue-8ff1cc.netlify.app/',
+  integrations: [preact()]
 });
