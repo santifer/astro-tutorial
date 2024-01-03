09167e160f8189f8438f69cac25caa1eedc3d5a6
santifer <almacen@santifer.me>
2024-01-03 17:59:44 +0100

    añadido rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://stirring-meringue-8ff1cc.netlify.app/',
+});
